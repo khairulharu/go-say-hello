f889c086e61dbf5337b34fd1e6fdb9ca44abcf19
khairulharu <khairulaswad719@gmail.com>
2023-08-19 11:46:11 +0800

    ubah

--- say-hello.go
+++ say-hello.go
@@ -1,5 +1,11 @@
 package gosayhello
 
+import "fmt"
+
 func SayHello() string {
 	return "halo berhasil buat modules"
+}
+
+func SayLord(name string) string {
+	return fmt.Sprintln("hallo lord", name)
 }
