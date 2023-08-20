fbd52e06d5f7b81c8a59829f9dbacd8aa62f5dca
khairulharu <khairulaswad719@gmail.com>
2023-08-20 12:05:48 +0800

    bracking changes

--- say-hello.go
+++ say-hello.go
@@ -9,3 +9,7 @@ func SayHello() string {
 func SayLord(name string) string {
 	return fmt.Sprintln("hallo lord", name)
 }
+
+func SayNumber(number int) any {
+	return fmt.Sprintln("namor yang kamu masukkna", number)
+}
